94272982771f7bac3fbcd8d475a8551b8e2ffd87
Olivier <oliv.nguyen@hotmail.fr>
2015-01-07 00:11:34 +0100

    Add Navigation Drawer (Not finish but working)

---
+++ app/src/main/java/fr/poucedor/poucedor/BaseActivity.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2014 Antonio Leiva Gordillo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.poucedor.poucedor;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public abstract class BaseActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawer.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(Gravity.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    protected abstract int getLayoutResource();
+
+    protected void setActionBarIcon(int iconRes) {
+        toolbar.setNavigationIcon(iconRes);
+    }
+}
--- app/src/main/java/fr/poucedor/poucedor/MapActivity.java
+++ app/src/main/java/fr/poucedor/poucedor/MapActivity.java
@@ -4,20 +4,46 @@ import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import android.os.Bundle;
-import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapController;
 
-public class MapActivity extends Activity {
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
+import android.os.Bundle;
+import android.graphics.drawable.Drawable;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MapActivity extends BaseActivity {
 
     MyItemizedOverlay myItemizedOverlay = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+
+        //setContentView(R.layout.activity_map);
+        setActionBarIcon(R.drawable.ic_ab_drawer);
+
         MapView mapView = (MapView) findViewById(R.id.mapview);
+
+        MapController mapController = new MapController(mapView);
+
+        mapController.setZoom(7);
+        mapView.setMinZoomLevel(4);
+
+        double lat = 48.8534100 * 1000000;
+        double lon = 2.3488000 * 1000000;
+        GeoPoint p = new GeoPoint((int) lat, (int) lon);
+        mapController.animateTo(p);
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
 
         Drawable marker=getResources().getDrawable(android.R.drawable.star_big_on);
         int markerWidth = marker.getIntrinsicWidth();
@@ -31,9 +57,13 @@ public class MapActivity extends Activity {
 
         GeoPoint myPoint1 = new GeoPoint(0*1000000, 0*1000000);
         myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
-        GeoPoint myPoint2 = new GeoPoint(50*1000000, 50*1000000);
+        GeoPoint myPoint2 = new GeoPoint((int)48.8534100*1000000, (int)2.3488000*1000000);
         myItemizedOverlay.addItem(myPoint2, "myPoint2", "myPoint2");
 
+    }
+
+    @Override protected int getLayoutResource() {
+        return R.layout.activity_map;
     }
 
 }
