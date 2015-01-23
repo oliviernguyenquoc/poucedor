daaf01c3d3d01d476f6edb176da304ef20078ba5
Olivier <oliv.nguyen@hotmail.fr>
2015-01-23 10:17:43 +0100

    Add location on floating action button feature (Imported from another branch in development)

--- app/src/main/java/fr/poucedor/poucedor/MapActivity.java
+++ app/src/main/java/fr/poucedor/poucedor/MapActivity.java
@@ -46,36 +46,7 @@ public class MapActivity extends BaseActivity implements LoaderManager.LoaderCal
 
         setContentView(R.layout.activity_map);
 
-        MapView mapView = (MapView) findViewById(R.id.mapview);
-
-        MapController mapController = new MapController(mapView);
-
-        mapController.setZoom(7);
-        mapView.setMinZoomLevel(4);
-
-        // Center over France
-        double lat = 51 * 1000000;
-        double lon = -5 * 1000000;
-        GeoPoint p = new GeoPoint((int) lat, (int) lon);
-        mapController.animateTo(p);
-
-        mapView.setTileSource(TileSourceFactory.MAPNIK);
-        mapView.setMultiTouchControls(true);
-
-        Drawable marker = getResources().getDrawable(R.drawable.ic_drawer_place);
-        int markerWidth = marker.getIntrinsicWidth();
-        int markerHeight = marker.getIntrinsicHeight();
-        marker.setBounds(0, markerHeight, markerWidth, 0);
-
-        ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
-
-        myItemizedOverlay = new MyItemizedOverlay(marker, resourceProxy);
-        mapView.getOverlays().add(myItemizedOverlay);
-
-        GeoPoint myPoint1 = new GeoPoint(0 * 1000000, 0 * 1000000);
-        myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
-        GeoPoint myPoint2 = new GeoPoint((int) 48.8534100 * 1000000, (int) 2.3488000 * 1000000);
-        myItemizedOverlay.addItem(myPoint2, "myPoint2", "myPoint2");
+        setMap();
 
         setFloatingActionButton();
 
@@ -125,20 +96,73 @@ public class MapActivity extends BaseActivity implements LoaderManager.LoaderCal
 
     }
 
+    public void setMap(){
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+
+        MapController mapController = new MapController(mapView);
+
+        mapController.setZoom(7);
+        mapView.setMinZoomLevel(4);
+
+        // Center over France
+        double lat = 51 * 1000000;
+        double lon = -5 * 1000000;
+        GeoPoint p = new GeoPoint((int) lat, (int) lon);
+        mapController.animateTo(p);
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setMultiTouchControls(true);
+
+        Drawable marker = getResources().getDrawable(R.drawable.ic_drawer_place);
+        int markerWidth = marker.getIntrinsicWidth();
+        int markerHeight = marker.getIntrinsicHeight();
+        marker.setBounds(0, markerHeight, markerWidth, 0);
+
+        ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+
+        myItemizedOverlay = new MyItemizedOverlay(marker, resourceProxy);
+        mapView.getOverlays().add(myItemizedOverlay);
+
+        GeoPoint myPoint1 = new GeoPoint(0 * 1000000, 0 * 1000000);
+        myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
+        GeoPoint myPoint2 = new GeoPoint((int) 48.8534100 * 1000000, (int) 2.3488000 * 1000000);
+        myItemizedOverlay.addItem(myPoint2, "myPoint2", "myPoint2");
+    }
+
     public void setFloatingActionButton(){
         ListView listView = (ListView) findViewById(android.R.id.list);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.attachToListView(listView);
-        if (android.os.Build.VERSION.SDK_INT >=21){
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
             fab.setColorRipple(getResources().getColor(R.color.ripple));
         }
         findViewById(R.id.floatingActionButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MapActivity.this, "Clicked Floating Action Button", Toast.LENGTH_SHORT).show();
+                findMyLocation();
             }
         });
     }
+
+    public void findMyLocation() {
+        System.out.println("-----------------------ETAPE 1----------------------");
+        GPSTracker gps = new GPSTracker(this);
+
+        if (gps.canGetLocation()) {
+            System.out.println("-----------------------ETAPE 2----------------------");
+            GeoPoint myPoint1 = new GeoPoint((int) gps.getLatitude() * 1000000, (int) gps.getLongitude() * 1000000);
+            myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
+            System.out.println("-----------------------ETAPE 3----------------------");
+            gps.stopUsingGPS();
+            System.out.println("-----------------------ETAPE 4----------------------");
+        } else {
+            Toast.makeText(MapActivity.this, R.string.gps_not_enable, Toast.LENGTH_SHORT).show();
+        }
+        setFloatingActionButton();
+
+        getLoaderManager().initLoader(URL_LOADER, null, this);
+    }
+
 
     @Override
     protected int getSelfNavDrawerItem() {
